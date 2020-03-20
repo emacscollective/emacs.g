cd0a0224c184ad8d3400f697b55e1248122abca3
Jonas Bernoulli <jonas@bernoul.li>
2020-03-20 23:37:24 +0100

    Emacsg: init: Add early-init.el

---
+++ early-init.el
@@ -0,0 +1,9 @@
+;;; early-init.el --- earliest birds               -*- lexical-binding: t -*-
+
+(setq package-enable-at-startup nil)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; indent-tabs-mode: nil
+;; End:
+;;; early-init.el ends here
