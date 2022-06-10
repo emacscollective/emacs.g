02df15b7c4f0536985db61437601fbadbf5e7eee
Jonas Bernoulli <jonas@bernoul.li>
2022-06-10 19:54:10 +0200

    Assimilate compat

--- early-init.el
+++ early-init.el
@@ -3,6 +3,7 @@
 (setq load-prefer-newer t)
 
 (let ((dir (file-name-directory (or load-file-name buffer-file-name))))
+  (add-to-list 'load-path (expand-file-name "lib/compat" dir))
   (add-to-list 'load-path (expand-file-name "lib/packed" dir))
   (add-to-list 'load-path (expand-file-name "lib/auto-compile" dir)))
 (require 'auto-compile)
