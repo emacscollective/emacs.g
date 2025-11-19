703274969ea809aabc53e48515578f7d26f05ace
Jonas Bernoulli <jonas@bernoul.li>
2025-11-19 16:00:13 +0100

    Emacsg: Initialize Borg in early-init.el

--- init.el
+++ init.el
@@ -1,14 +1,6 @@
 ;;; init.el --- user-init-file                    -*- lexical-binding: t -*-
 ;;; Early birds
 (progn ;     startup
-  (defvar before-user-init-time (current-time)
-    "Value of `current-time' when Emacs begins loading `user-init-file'.")
-  (message "Loading Emacs...done (%.3fs)"
-           (float-time (time-subtract before-user-init-time
-                                      before-init-time)))
-  (setq user-init-file (or load-file-name buffer-file-name))
-  (setq user-emacs-directory (file-name-directory user-init-file))
-  (message "Loading %s..." user-init-file)
   (setq inhibit-startup-buffer-menu t)
   (setq inhibit-startup-screen t)
   (setq inhibit-startup-echo-area-message "locutus")
@@ -19,11 +11,6 @@
   (when (fboundp 'tool-bar-mode)
     (tool-bar-mode 0))
   (menu-bar-mode 0))
-
-(eval-and-compile ; `borg'
-  (add-to-list 'load-path (expand-file-name "lib/borg" user-emacs-directory))
-  (require 'borg)
-  (borg-initialize))
 
 (eval-and-compile ; `use-package'
   (setopt use-package-enable-imenu-support t)
