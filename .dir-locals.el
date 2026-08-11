278106d49b72c7209979bbac85bce2eadde5b564
Jonas Bernoulli <jonas@bernoul.li>
2026-08-11 16:29:28 +0200

    Emacsg: Remove undo-tree settings from .dir-locals.el
    We no longer use that package.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,4 @@
 (
  (auto-mode-alist
   ("\\.\\(borgconfig\\|gitremotes\\)\\'" . gitconfig-mode))
- ("lib/undo-tree"
-  (emacs-lisp-mode
-   (outline-regexp . ";;;\\(;* [^=\t\n]\\|###autoload\\)\\|(")))
  )
